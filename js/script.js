819992d100f63388d79da776e09553d29e46c688
OlyaJoss <65188310+OlyaJoss@users.noreply.github.com>
2021-05-30 14:26:35 +0300

    готовая верстка

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let sortButton = document.querySelector('.filter-icon');
+sortButton.addEventListener('click', (event) => {
+    sortButton.classList.toggle('filter-icon--down')
+    sortButton.classList.toggle('filter-icon--up')
+
+    // TODO add sort function
+
+})
